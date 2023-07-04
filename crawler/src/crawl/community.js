9d3bb4df805d6dce61af4a234d909fd504fdf3de
Domenic Horner <domenic@tgxn.net>
2023-07-04 18:15:00 +0800

    active users

--- crawler/src/crawl/community.js
+++ crawler/src/crawl/community.js
@@ -78,15 +78,15 @@ export default class CommunityCrawler {
     await storage.community.setTrackedAttribute(
       this.crawlDomain,
       communityPart,
-      "users_active_week",
-      community.counts.users_active_week
+      "users_active_day",
+      community.counts.users_active_day
     );
 
     await storage.community.setTrackedAttribute(
       this.crawlDomain,
       communityPart,
-      "users_active_day",
-      community.counts.users_active_day
+      "users_active_week",
+      community.counts.users_active_week
     );
 
     await storage.community.setTrackedAttribute(
