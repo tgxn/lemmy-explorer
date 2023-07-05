48be442434ffd254f5b2b0bf9c9f2021bfe33188
Domenic Horner <domenic@tgxn.net>
2023-07-05 19:07:14 +0800

    connect crons

--- crawler/src/bin/worker.js
+++ crawler/src/bin/worker.js
@@ -13,6 +13,8 @@ import CrawlKBin from "../crawl/kbin.js";
 
 import { AGED_CRON_EXPRESSION, UPTIME_CRON_EXPRESSION } from "../lib/const.js";
 
+import storage from "../storage.js";
+
 /**
  * Queue worker and CRON tasks are started here.
  */
@@ -41,20 +43,32 @@ export default async function startWorker(startWorkerName = null) {
   else if (startWorkerName == "cron") {
     logging.info("Creating Cron Tasks (Aged/Uptime)");
 
-    const agedTask = cron.schedule(AGED_CRON_EXPRESSION, async () => {
-      const aged = new CrawlAged();
-      aged.createJobs();
-    });
+    const agedTask = cron.schedule(
+      AGED_CRON_EXPRESSION,
+      async () => {
+        await storage.connect();
+        const aged = new CrawlAged();
+        await aged.createJobs();
+        await storage.close();
+      },
+      {
+        runOnInit: true,
+      }
+    );
 
     const uptimeTask = cron.schedule(UPTIME_CRON_EXPRESSION, async () => {
+      await storage.connect();
       const uptime = new CrawlUptime();
-      uptime.crawl();
+      await uptime.crawl();
+      await storage.close();
     });
 
     // shares AGED_CRON_EXPRESSION since it should happen at the same time (hourly check if they need a re-scan)
     const kbinTask = cron.schedule(AGED_CRON_EXPRESSION, async () => {
+      await storage.connect();
       const kbinScan = new CrawlKBin();
       await kbinScan.createJobsAllKBin();
+      await storage.close();
     });
 
     // if (AUTO_UPLOAD_S3) {
