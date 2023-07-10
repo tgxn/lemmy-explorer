73a1771aa240be7ccf55940933446a7f0a5e8672
Domenic Horner <domenic@tgxn.net>
2023-07-10 22:44:28 +0800

    update logging

--- crawler/ecosystem.config.cjs
+++ crawler/ecosystem.config.cjs
@@ -1,15 +1,8 @@
 const defaultOptions = {
-  // log_type: "raw",
-
-  // log_file: "/logs/combined.log",
-  combine_logs: true,
-
+  log_type: "raw",
   output: "/logs/out.log",
-  error: "/logs/error.log",
-  log: "/logs/combined.outerr.log",
-
-  // autorestart: true,
   script: "index.js",
+  exec_mode: "fork",
 };
 
 module.exports = {
@@ -18,35 +11,30 @@ module.exports = {
       ...defaultOptions,
       name: "scheduler",
       args: ["-w", "cron"],
-      exec_mode: "fork",
       instances: 1,
     },
     {
       ...defaultOptions,
       name: "crawl-instance",
       args: ["-w", "instance"],
-      exec_mode: "fork",
       instances: 4,
     },
     {
       ...defaultOptions,
       name: "crawl-community",
       args: ["-w", "community"],
-      exec_mode: "fork",
       instances: 4,
     },
     {
       ...defaultOptions,
       name: "crawl-one-community",
       args: ["-w", "single"],
-      exec_mode: "fork",
       instances: 2,
     },
     {
       ...defaultOptions,
       name: "crawl-kbin",
       args: ["-w", "kbin"],
-      exec_mode: "fork",
       instances: 2,
     },
   ],
