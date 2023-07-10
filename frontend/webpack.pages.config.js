9384d3cd18c3e85258458fdba465954585135824
Domenic Horner <domenic@tgxn.net>
2023-07-10 17:12:33 +0800

    schedule build

--- frontend/webpack.pages.config.js
+++ frontend/webpack.pages.config.js
@@ -4,8 +4,6 @@ const prod = require("./webpack.prod.config.js");
 
 module.exports = merge(prod, {
   output: {
-    path: path.join(__dirname, "dist"),
     publicPath: "./",
-    clean: true,
   },
 });
