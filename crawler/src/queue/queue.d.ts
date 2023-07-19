eda76643f640b45e0953cc66c02441bc7600d000
Domenic Horner <domenic@tgxn.net>
2023-07-19 13:15:03 +0800

    stupid and annoying 1/10

--- crawler/src/queue/queue.d.ts
+++ crawler/src/queue/queue.d.ts
@@ -1,10 +1,11 @@
 export default class BaseQueue {
-    constructor(isWorker: any, queueName: any, jobProcessor: any);
-    queueName: any;
-    jobProcessor: any;
-    logPrefix: string;
-    queue: Queue<any>;
-    createJob(jobId: any, jobData: any, onSuccess?: any): Promise<void>;
-    process(): void;
+  constructor(isWorker: any, queueName: any, jobProcessor: any);
+  queueName: any;
+  jobProcessor: any;
+  logPrefix: string;
+  queue: Queue<any>;
+  createJob(jobId: any, jobData: any, onSuccess?: any): Promise<void>;
+  process(): void;
 }
+
 import Queue from "bee-queue";
