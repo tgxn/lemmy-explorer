f89f1b8c41c622f8b4fb78b9de821ead881ed7a0
Domenic Horner <domenic@tgxn.net>
2023-07-23 12:01:38 +0800

    clean up instancegrowth imports

--- frontend/src/components/InstanceView/InstanceUserGrowth.jsx
+++ frontend/src/components/InstanceView/InstanceUserGrowth.jsx
@@ -1,44 +1,8 @@
 import React from "react";
-import { connect } from "react-redux";
 
-import Moment from "react-moment";
 import moment from "moment";
 
-import {
-  CartesianGrid,
-  Legend,
-  ResponsiveContainer,
-  Scatter,
-  ScatterChart,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
-
-import Avatar from "@mui/joy/Avatar";
-import Card from "@mui/joy/Card";
-import CardContent from "@mui/joy/CardContent";
-import Typography from "@mui/joy/Typography";
-import Divider from "@mui/joy/Divider";
-import CardOverflow from "@mui/joy/CardOverflow";
-import Box from "@mui/joy/Box";
-
-import Link from "@mui/joy/Link";
-import IconButton from "@mui/joy/IconButton";
-
-import PersonIcon from "@mui/icons-material/Person";
-import MessageIcon from "@mui/icons-material/Message";
-import ForumIcon from "@mui/icons-material/Forum";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-import TrendingUpIcon from "@mui/icons-material/TrendingUp";
-import ThumbDownIcon from "@mui/icons-material/ThumbDown";
-import HomeIcon from "@mui/icons-material/Home";
-
-import { TinyNumber, BannerImage } from "../Shared/Display";
-
-import { CopyLink } from "../Shared/Link";
-
-import { setHomeInstance } from "../../reducers/configReducer";
+import { ResponsiveContainer, Scatter, ScatterChart, Tooltip, XAxis, YAxis } from "recharts";
 
 export default function InstanceUserGrowth({ userSeries }) {
   const minUsers = Number(userSeries[0].value);
