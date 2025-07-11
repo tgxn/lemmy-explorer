18fd7cc48a51edd62801746a2f9cedd3bffb57d3
tgxn <domenic@tgxn.net>
2025-07-12 02:40:03 +0800

    versions, create basic tests

---
+++ crawler/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  testMatch: ["**/test/**/*.test.ts"],
+};
