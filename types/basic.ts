1f628a0d9cf7a88ced1ff6a8ae5c5b444b1b2ca7
tgxn <domenic@tgxn.net>
2025-07-13 16:38:51 +0800

    basic types

--- types/basic.ts
+++ types/basic.ts
@@ -11,17 +11,3 @@ export type BaseURL = string;
  * Example: `https://lemmy.example.org/c/example_community`, `https://lemmy.example.org/u/example_user`
  */
 export type ActorID = string;
-
-/**
- * A string representing a time in the format used by Lemmy.
- *
- * Example: `2023-06-13T01:51:01.061316`
- */
-export type LemmyTimeString = string;
-
-/**
- * A string representing a time in the format used by Lemmy.
- *
- * Example: `2023-06-15T05:02:07.318980Z`
- */
-export type LemmyTimeStringZ = string;
