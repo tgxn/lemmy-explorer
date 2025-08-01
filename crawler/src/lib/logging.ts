73657aa39d9a8a2c4a064df2c26b75d413da69e4
tgxn <domenic@tgxn.net>
2025-08-02 04:18:58 +0800

    logging re-work

--- crawler/src/lib/logging.ts
+++ crawler/src/lib/logging.ts
@@ -1,38 +1,64 @@
-// import { LOG_LEVEL } from "./const";
+import pino from "pino";
+import pretty from "pino-pretty";
 
-// pm2 already writes logs, just make them pretty
+import { LOG_LEVEL } from "./const";
 
-const logging = {
-  silly: (...args: any) => {},
-  // trace: console.trace,
-  // verbose: console.trace,
-  debug: console.debug,
-  info: console.info,
-  warn: console.warn,
-  error: console.error,
-  fatal: console.error,
+const stream = pretty({
+  colorize: true,
+  translateTime: "yyyy-mm-dd HH:MM:ss.SSS",
+  ignore: "pid,hostname",
+});
 
-  formatDuration: (ms: number) => {
-    const milliseconds = (ms % 1000).toString().padStart(3, "0");
+const baseLogger = pino({ level: LOG_LEVEL }, stream);
 
-    const seconds = Math.floor(ms / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
+const formatDuration = (ms: number) => {
+  const milliseconds = (ms % 1000).toString().padStart(3, "0");
 
-    if (months > 0) {
-      return `${months}mo+`;
-    } else if (days > 0) {
-      return `${days}d ${hours % 24}h ${minutes % 60}m ${seconds % 60}.${milliseconds}s`;
-    } else if (hours > 0) {
-      return `${hours}h ${minutes % 60}m ${seconds % 60}.${milliseconds}s`;
-    } else if (minutes > 0) {
-      return `${minutes}m ${seconds % 60}.${milliseconds}s`;
-    } else {
-      return `${seconds}.${milliseconds}s`;
-    }
+  const seconds = Math.floor(ms / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+
+  if (months > 0) {
+    return `${months}mo+`;
+  } else if (days > 0) {
+    return `${days}d ${hours % 24}h ${minutes % 60}m ${seconds % 60}.${milliseconds}s`;
+  } else if (hours > 0) {
+    return `${hours}h ${minutes % 60}m ${seconds % 60}.${milliseconds}s`;
+  } else if (minutes > 0) {
+    return `${minutes}m ${seconds % 60}.${milliseconds}s`;
+  } else {
+    return `${seconds}.${milliseconds}s`;
+  }
+};
+
+type ILogFunction = (message: string, ...args: any[]) => void;
+
+type ILogging = {
+  trace: ILogFunction;
+  debug: ILogFunction;
+  info: ILogFunction;
+  warn: ILogFunction;
+  error: ILogFunction;
+  fatal: ILogFunction;
+  table: (data: any, columns?: string[]) => void;
+  formatDuration: (ms: number) => string;
+};
+
+const logging: ILogging = {
+  trace: baseLogger.trace.bind(baseLogger),
+  debug: baseLogger.debug.bind(baseLogger),
+  info: baseLogger.info.bind(baseLogger),
+  warn: baseLogger.warn.bind(baseLogger),
+  error: baseLogger.error.bind(baseLogger),
+  fatal: baseLogger.fatal.bind(baseLogger),
+
+  table: (data: any, columns?: string[]) => {
+    baseLogger.info("table\n" + JSON.stringify(data, null, 2));
+    console.table(data, columns);
   },
+  formatDuration: formatDuration,
 };
 
 export default logging;
