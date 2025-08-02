8a67e54c75fa7f50f765fd6e4e0e59a605ad0514
tgxn <domenic@tgxn.net>
2025-08-03 01:01:03 +0800

    Refactor: comment out unused components and logging in InstanceContentGrowth, InstanceOverview, and LineGraph

--- frontend/src/components/InstanceView/InstanceContentGrowth.tsx
+++ frontend/src/components/InstanceView/InstanceContentGrowth.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+// import React from "react";
 
-import LineGraph from "../Shared/LineGraph";
+// import LineGraph from "../Shared/LineGraph";
 
-export default function InstanceContentGrowth({ userSeries }) {
-  console.log("userSeries", userSeries);
+// export default function InstanceContentGrowth({ userSeries }) {
+//   console.log("userSeries", userSeries);
 
-  return <LineGraph dataSeries={userSeries} />;
-}
+//   return <LineGraph dataSeries={userSeries} />;
+// }
