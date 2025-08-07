c029a09afa6d5ffb10db4a1ec8772f125813f9d7
tgxn <domenic@tgxn.net>
2025-08-07 14:17:22 +0800

    adjust waits

--- frontend/test/config/test.utils.ts
+++ frontend/test/config/test.utils.ts
@@ -7,11 +7,14 @@ export const expect = test.expect;
 // Screenshots contain the test name and are saved in the `output/screens` directory.
 test.afterEach(async ({ page }, testInfo) => {
   const testName = testInfo.title.replace(/[^a-zA-Z0-9]/g, "_").toLowerCase();
-
   const path = `output/screens/${testName}.png`;
 
-  // wait 1000 ms to ensure the page is fully rendered
-  await page.waitForTimeout(7500);
+  // to ensure the page is fully rendered
+  await Promise.all([
+    page.waitForLoadState("networkidle"),
+    page.waitForFunction(() => document.readyState === "complete"),
+  ]);
+  await page.waitForTimeout(500);
 
   await page.screenshot({ path });
   console.log("Screenshot saved to", path);
