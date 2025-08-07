9d4d5c274bd6ed0a470241eee6755b450b53bb15
tgxn <domenic@tgxn.net>
2025-08-07 11:45:59 +0800

    update screens

--- frontend/test/spec/main-screenshots.spec.ts
+++ frontend/test/spec/main-screenshots.spec.ts
@@ -1,26 +1,38 @@
 import { test, expect } from "@playwright/test";
 import "../config/test.utils";
 
-test("main: instances page screenshot", async ({ page }) => {
-  await page.setViewportSize({ width: 800, height: 600 });
+test("main: (1080p) instances page screenshot", async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
   await page.goto("/", { waitUntil: "networkidle" });
   await expect(page.locator('input[placeholder="Filter Instances"]')).toBeVisible();
 });
 
-test("main: communities page screenshot", async ({ page }) => {
-  await page.setViewportSize({ width: 1024, height: 768 });
+test("main: (1080p) communities page screenshot", async ({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
+  await page.goto("/communities", { waitUntil: "networkidle" });
+  await expect(page.locator('input[placeholder="Filter Communities"]')).toBeVisible();
+});
+
+test("main: (mobile) instances page screenshot", async ({ page }) => {
+  await page.setViewportSize({ width: 454, height: 917 });
+  await page.goto("/", { waitUntil: "networkidle" });
+  await expect(page.locator('input[placeholder="Filter Instances"]')).toBeVisible();
+});
+
+test("main: (mobile) communities page screenshot", async ({ page }) => {
+  await page.setViewportSize({ width: 454, height: 917 });
   await page.goto("/communities", { waitUntil: "networkidle" });
   await expect(page.locator('input[placeholder="Filter Communities"]')).toBeVisible();
 });
 
 test("main: instance view overview lemmy.world", async ({ page }) => {
-  await page.setViewportSize({ width: 800, height: 600 });
+  await page.setViewportSize({ width: 1920, height: 1080 });
   await page.goto("/instance/lemmy.world", { waitUntil: "networkidle" });
   await expect(page).toHaveURL(/\/instance\/lemmy\.world/);
 });
 
 test("main: instance view stats lemmy.world", async ({ page }) => {
-  await page.setViewportSize({ width: 800, height: 600 });
+  await page.setViewportSize({ width: 1920, height: 1080 });
   await page.goto("/instance/lemmy.world/user-growth", { waitUntil: "networkidle" });
   await expect(page).toHaveURL(/\/instance\/lemmy\.world\/user-growth/);
 });
