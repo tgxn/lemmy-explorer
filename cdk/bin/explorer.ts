3788bbfba0f54d47ff36ec89f52243ca458e55ab
tgxn <domenic@tgxn.net>
2026-01-18 22:20:38 +0800

    hmm dev

--- cdk/bin/explorer.ts
+++ cdk/bin/explorer.ts
@@ -5,24 +5,31 @@ import * as cdk from "aws-cdk-lib";
 import { CertStack } from "../lib/cert-stack";
 import { FrontendStack } from "../lib/frontend-stack";
 import { BuildStack } from "../lib/build-stack";
+import { RolesStack } from "../lib/roles-stack";
 
 import config from "../config.json";
 
 const app = new cdk.App();
 
-const certStack = new CertStack(app, `LemmyExplorerUS-Cert-${config.environment}`, {
+const certStack = new CertStack(app, `cdk-${config.environment}-LemmyExplorer-Cert`, {
   env: { region: "us-east-1", account: config.account },
   crossRegionReferences: true,
 });
 
-new BuildStack(app, `LemmyExplorerUS-Build-${config.environment}`, {
+const buildStack = new BuildStack(app, `cdk-${config.environment}-LemmyExplorer-Build`, {
   env: { region: "us-east-1", account: config.account },
 });
 
-const frontendStack = new FrontendStack(app, `LemmyExplorerUS-Frontend-${config.environment}`, {
+const rolesStack = new RolesStack(app, `cdk-${config.environment}-LemmyExplorer-Roles`, {
+  env: { region: "us-east-1", account: config.account },
+  buildBucket: buildStack.buildBucket,
+});
+
+rolesStack.addDependency(buildStack);
+
+const frontendStack = new FrontendStack(app, `cdk-${config.environment}-LemmyExplorerUS-Frontend`, {
   env: { region: "us-east-1", account: config.account },
   cert: certStack.cert,
-  // crossRegionReferences: true,
 });
 
 frontendStack.addDependency(certStack);
--- cdk/lib/build-stack.ts
+++ cdk/lib/build-stack.ts
@@ -6,23 +6,25 @@ import { Construct } from "constructs";
 import { AnyPrincipal, Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class BuildStack extends Stack {
+  public readonly buildBucket: s3.Bucket;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // Build Bucket
-    const buildBucket = new s3.Bucket(this, "BuildBucket", {
+    this.buildBucket = new s3.Bucket(this, "BuildBucket", {
       publicReadAccess: false,
       encryption: s3.BucketEncryption.S3_MANAGED,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
     // Policy to deny access to the bucket via unencrypted connections
-    buildBucket.addToResourcePolicy(
+    this.buildBucket.addToResourcePolicy(
       new PolicyStatement({
         effect: Effect.DENY,
         principals: [new AnyPrincipal()],
         actions: ["s3:*"],
-        resources: [buildBucket.bucketArn],
+        resources: [this.buildBucket.bucketArn],
         conditions: {
           Bool: { "aws:SecureTransport": "false" },
         },
