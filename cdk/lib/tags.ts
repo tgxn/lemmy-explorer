7e81905249aa5a2fff0f4cfbae7714d3e934df0c
tgxn <domenic@tgxn.net>
2026-01-20 22:28:33 +0800

    tagging please

---
+++ cdk/lib/tags.ts
@@ -0,0 +1,33 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import config from "../config.json";
+
+export interface ResourceTagsConfig {
+  stackName: string;
+  purpose: "certificate" | "storage" | "access" | "distribution";
+  billingCategory: "certificate" | "storage" | "iam" | "cdn";
+  service: "acm" | "s3" | "iam" | "cloudfront";
+}
+
+const baseTags = {
+  Solution: "LemmyExplorer",
+  Environment: config.environment,
+  ManagedBy: "CDK",
+  Project: "lemmy-scraper",
+};
+
+export function applyTags(construct: Construct, tagsConfig: ResourceTagsConfig): void {
+  const { stackName, purpose, billingCategory, service } = tagsConfig;
+
+  const tags = {
+    ...baseTags,
+    Stack: stackName,
+    Purpose: purpose,
+    BillingCategory: billingCategory,
+    Service: service,
+  };
+
+  Object.entries(tags).forEach(([key, value]) => {
+    cdk.Tags.of(construct).add(key, value);
+  });
+}
